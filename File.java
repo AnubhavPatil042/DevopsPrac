cf95c515cee2f272742c446d1360bf1cabb82b17
AnubhavPatil042 <2024.anubhav.patil@ves.ac.in>
2025-04-25 12:28:58 +0530

    f

--- File.java
+++ File.java
@@ -1,21 +1,21 @@
 public class File {
 
     static int areaRect(int length, int width){
-        return length* width;
+        return length * width;
     }
 
     static int perimeterRect(int length, int width){
-        return (2*length*width);
+        return 2 * (length + width);  
     }
+
     public static void main(String[] args) {
         int l = 10;
         int w = 5;
 
-        int area = areaRect(l,w);
-        int per = perimeterRect(l,w);
+        int area = areaRect(l, w);
+        int per = perimeterRect(l, w);
 
-        System.out.println("Area is"+area);
-        System.out.println("Perimeter is: "+ per);
-
+        System.out.println("Area is: " + area);
+        System.out.println("Perimeter is: " + per);
     }
 }
