ec838b9d848507876e806de4a5be3c01968ef761
AnubhavPatil042 <2024.anubhav.patil@ves.ac.in>
2025-04-25 12:19:15 +0530

    Update File.java

--- File.java
+++ File.java
@@ -1,15 +1,21 @@
 public class File {
 
-    static int areaRect(int length, int breadth){
-        return length* breadth;
+    static int areaRect(int length, int width){
+        return length* width;
     }
 
+    static int perimeterRect(int length, int width){
+        return (2*length*width);
+    }
     public static void main(String[] args) {
-        
+        int l = 10;
+        int w = 5;
 
-        int area = areaRect(10,12);
+        int area = areaRect(l,w);
+        int per = perimeterRect(l,w);
 
-        System.out.println(area);
+        System.out.println("Area is"+area);
+        System.out.println("Perimeter is: "+ per);
 
     }
 }
