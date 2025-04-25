8f1fe1bd01fe2e1419fc2982195a4d2343a267b4
AnubhavPatil042 <2024.anubhav.patil@ves.ac.in>
2025-04-25 11:59:58 +0530

    Add File.java

---
+++ File.java
@@ -0,0 +1,15 @@
+public class File {
+
+    static int areaRect(int length, int breadth){
+        return length* breadth;
+    }
+
+    public static void main(String[] args) {
+        
+
+        int area = areaRect(10,12);
+
+        System.out.println(area);
+
+    }
+}
